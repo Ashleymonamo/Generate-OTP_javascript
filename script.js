d7a7e6880884180e7bff7209638d069a5492179b
Ashleymonamo <ashleymonamo@gmail.com>
2023-10-13 04:59:55 -0800

    Cleaned code

--- script.js
+++ script.js
@@ -29,38 +29,4 @@ function OTPCode() {
         document.getElementById("outpt").value=optValue;
         console.log(optValue);
     }
-    // const otpLength=6;
-    // let optValue="";
-    // let i;
-  
-    // for (i=0;i<otpLength;)
-    // {
-    //     optValue +=Math.floor(Math.random()*9)
-    //     i++;
-
-    // }
-    // let items=[{Pin:optValue}];
-    // localStorage.setItem("OTP",JSON.stringify(items));
-    // console.log(optValue);
-   
-    // return optValue;
-    
 }
-// function displayPin()
-// {
-//     let storage= localStorage.getItem("Pin");
-//     let data=JSON.parse(storage);   
-//     document.getElementById("labelotp").value=data[0].Pin
-// }
-//ALPAHNUMERIC CODE 
-// function generateAlphanumericOTP(length) {
-//     const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-//     let otp = '';
-  
-//     for (let i = 0; i < length; i++) {
-//       const randomIndex = Math.floor(Math.random() * characters.length);
-//       otp += characters[randomIndex];
-//     }
-//   console.log(otp);
-//     return otp;
-//   }

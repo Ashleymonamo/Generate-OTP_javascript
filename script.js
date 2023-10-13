c5ef62086d54bbc3ab2ae8d957b45937fb58f723
Ashleymonamo <ashleymonamo@gmail.com>
2023-10-13 03:10:02 -0800

    Calculation Update

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
 console.log("running")
 function OTPCode() {
-    const otpLength=6;
-    let optValue="";
-    let i;
-    let pin=document.getElementById("otp");
-    for (i=0;i<otpLength;)
-    {
-        optValue +=Math.floor(Math.random()*9)
-        i++;
-
+    let opt=document.getElementById("pin").value
+    if (opt=="Alpha-numeric") {
+        const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        let otp = '';
+      let length=6;
+        for (let i = 0; i < length; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          otp += characters[randomIndex];
+        }
+        let items=[{Pin:otp}];
+        localStorage.setItem("OTP",JSON.stringify(items));
+        
+      console.log(otp);   
     }
-    console.log(optValue);
-    pin=optValue;
-    return optValue;
+    else if (opt=="Numeric")
+    {
+        const otpLength=6;
+        let optValue="";
+        let i;
+      
+        for (i=0;i<otpLength;)
+        {
+            optValue +=Math.floor(Math.random()*9)
+            i++;
+    
+        }
+        let items=[{Pin:optValue}];
+        localStorage.setItem("OTP",JSON.stringify(items));
+        console.log(optValue);
+    }
+    // const otpLength=6;
+    // let optValue="";
+    // let i;
+  
+    // for (i=0;i<otpLength;)
+    // {
+    //     optValue +=Math.floor(Math.random()*9)
+    //     i++;
+
+    // }
+    // let items=[{Pin:optValue}];
+    // localStorage.setItem("OTP",JSON.stringify(items));
+    // console.log(optValue);
+   
+    // return optValue;
     
 }
+// function displayPin()
+// {
+//     let storage= localStorage.getItem("Pin");
+//     let data=JSON.parse(storage);   
+//     document.getElementById("labelotp").value=data[0].Pin
+// }
+//ALPAHNUMERIC CODE 
+// function generateAlphanumericOTP(length) {
+//     const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+//     let otp = '';
+  
+//     for (let i = 0; i < length; i++) {
+//       const randomIndex = Math.floor(Math.random() * characters.length);
+//       otp += characters[randomIndex];
+//     }
+//   console.log(otp);
+//     return otp;
+//   }
